1080009d6151abab50f8dddb91586919b4f95990
apple11361 <apple113611361@gmail.com>
2017-08-18 15:23:32 +0800

    github.html

---
+++ assets/js/github.js
@@ -0,0 +1,71 @@
+$(document).ready(function()
+{
+	$("#cpp").click(function()
+	{
+		$("#cpp_content").slideToggle("slow");
+	});
+});
+
+$(document).ready(function()
+{
+	$("#qt").click(function()
+	{
+		$("#qt_content").slideToggle("slow");
+	});
+});
+
+$(document).ready(function()
+{
+	$("#c_optimization").click(function()
+	{
+		$("#c_optimization_content").slideToggle("slow");
+	});
+});
+
+$(document).ready(function()
+{
+	$("#fintech").click(function()
+	{
+		$("#fintech_content").slideToggle("slow");
+	});
+});
+
+$(document).ready(function()
+{
+	$("#network").click(function()
+	{
+		$("#network_content").slideToggle("slow");
+	});
+});
+
+$(document).ready(function()
+{
+	$("#chatbot").click(function()
+	{
+		$("#chatbot_content").slideToggle("slow");
+	});
+});
+
+$(document).ready(function()
+{
+	$("#c_sharp").click(function()
+	{
+		$("#c_sharp_content").slideToggle("slow");
+	});
+});
+
+$(document).ready(function()
+{
+	$("#compiler").click(function()
+	{
+		$("#compiler_content").slideToggle("slow");
+	});
+});
+
+$(document).ready(function()
+{
+	$("#unity").click(function()
+	{
+		$("#unity_content").slideToggle("slow");
+	});
+});
